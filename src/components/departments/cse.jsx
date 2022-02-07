8704243b6e45b53c498f47b457de336de7d29232
rahul <rahulparmar7654321@gmail.com>
2022-02-07 15:58:48 +0530

    aaa

--- src/components/departments/cse.jsx
+++ src/components/departments/cse.jsx
@@ -5,6 +5,7 @@ import Hod from "./hod"
 import Testimonial from "./testimonial"
 import DeptHeader from "./dept_head"
 import Faculty from "./faculty"
+import Gallery from "./gallery"
 export default function Cse(){
 
     return (
@@ -12,6 +13,12 @@ export default function Cse(){
         <DeptHeader />
         <Hod />
         <Faculty />
+        <div className="gallery_dept">
+        <Gallery/>
+        <Gallery/>
+        <Gallery/>
+
+        </div>
         <Testimonial />
         </>
     )
--- src/components/departments/dept_head.jsx
+++ src/components/departments/dept_head.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 
 import cse from "../../images/cse.jpg"
+import { BiSearchAlt } from "react-icons/bi";
+import { AiFillCaretDown } from "react-icons/ai";
 
 export default function DeptHeader() 
 {
     return(
         <div className="deptHead">
+            <div className="header">
+                <div className="title">
+                    <h3>Computer Science Department</h3>
+                </div>
+                <div className="searchbar">
+                    <input type="text" placeholder="Search here"/>
+                    <BiSearchAlt className="search_icon"/>
+                </div>
+            </div>
             <div className="imgsec">
                 <img src={cse} alt="cse" />
+            </div>
+            <div className="navbar_for_dept">
+            <p>FACULTY  <AiFillCaretDown/></p>
+            <p>SYLLABUS <AiFillCaretDown/></p>
+            <p>RESEARCH </p>
+            <p>NOTICES </p>
+            <p>ABOUT </p>
             </div>
         </div>
     )
